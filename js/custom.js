0e02f47e7183c61dc8fd1bacd2bf4ce97d6472cd
Miguel Gaspar <miguel.ferreira.gaspar@gmail.com>
2022-03-01 23:04:41 +0000

    Replacing photos

--- js/custom.js
+++ js/custom.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
         if ($(window).scrollTop() > 400) {
             $("#navigation").css("background-color","#0EB493");
         } else {
-            $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
+            $("#navigation").css("background-color","rgba(14, 180, 147, 0.8)");
         }
     });
 	
