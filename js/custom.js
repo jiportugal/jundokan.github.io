f99430a829cbc72503ef8c3d467c3cdc86c43fb5
Miguel Gaspar <miguel.gaspar@hitachivantara.com>
2022-04-06 16:34:57 +0100

    Adding a page for Gasshuku 2022

--- js/custom.js
+++ js/custom.js
@@ -98,10 +98,10 @@ $(document).ready(function () {
     $(".number-counters").appear(function () {
         $(".number-counters [data-to]").each(function () {
             var e = $(this).attr("data-to");
-            $(this).delay(6e3).countTo({
-                from: 50,
+            $(this).delay(3e3).countTo({
+                from: 0,
                 to: e,
-                speed: 3e3,
+                speed: 6e3,
                 refreshInterval: 50
             })
         })
